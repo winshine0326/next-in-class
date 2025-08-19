15cd7936f1d62d644ab8e8ae015d2c7f913d2572
winshine0326 <shane10340@gmail.com>
2025-08-19 12:26:13 +0900

    리스트 조회

---
+++ first-class/src/app/api/posts/route.js
@@ -0,0 +1,14 @@
+//app/api/posts/route.js
+import { getPosts, addPost } from "@/app/data/posts";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  //작성
+  const data = getPosts();
+  return NextResponse.json(data);
+}
+
+export async function POST(request) {
+  const { title, content } = await request.json();
+  //작성
+}
--- first-class/src/app/list/page.js
+++ first-class/src/app/list/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -6,31 +6,42 @@ import Link from "next/link";
 export default function ListPage() {
   const [posts, setPosts] = useState([]);
 
-  const fetchPosts = () => {
+  const fetchPosts = async () => {
     //fetch('/api/posts') 작성
-    fetch('/api/data')
-    //posts변경 
+    await fetch("/api/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+    //posts변경
   };
 
   useEffect(() => {
-   //fetchPosts 실행
-   fetchPosts();
+    //fetchPosts 실행
+    fetchPosts();
   }, []);
 
-  const handleDelete =   ( ) => {
+  const handleDelete = async (id) => {
     //delete api실행
+    await fetch(`/api/posts/${id}`, { method: "DELETE" });
+    fetchPosts();
   };
 
   return (
     <main>
       <h1>게시물 목록</h1>
       <ul>
-        {posts.map(post => (
+        {posts.map((post) => (
           <li key={post.id}>
             <Link href={`/post/${post.id}`}>{post.title}</Link>
             <div>{post.content}</div>
-            <button className="button" onClick={() => }>삭제</button>
-            <span className = "button"> <Link>수정</Link></span>
+            <button className="button" onClick={() => handleDelete(post.id)}>
+              삭제
+            </button>
+            <span className="button">
+              <Link href={`/post/${post.id}/edit`}>수정</Link>
+            </span>
           </li>
         ))}
       </ul>
