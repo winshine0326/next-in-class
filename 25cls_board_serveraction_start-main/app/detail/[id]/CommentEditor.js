88f0d4cbbeca581a639c938d16cbef44b763d62a
winshine0326 <shane10340@gmail.com>
2025-11-11 10:29:35 +0900

    2-1. CommentEditor 컴포넌트 수정

--- 25cls_board_serveraction_start-main/app/detail/[id]/CommentEditor.js
+++ 25cls_board_serveraction_start-main/app/detail/[id]/CommentEditor.js
@@ -1,27 +1,40 @@
+// /app/detail/[id]/CommentEditor.js
 "use client";
 import { createCommentAction } from "@/actions/create-comment";
 import { useActionState, useEffect } from "react";
 
-export default function CommentEditor({postId, author}){
-    
-     //폼요소 useRef로 저장 
-     //useActionState사용 
-    return(
-    <section  style={{maxWidth: '700px', margin: '30px auto'}}>
-        <form className="write-form"  > //폼속성 수정하기
-         
-          parent<input name="parent" value={postId}  type ="hidden" readOnly />
-          author<input name="author" value={author}  type="hidden" readOnly />
-          <input
+export default function CommentEditor({ postId, author }) {
+  const formRef = useRef(null);
+  const [state, formAction, isPending] = useActionState(
+    createCommentAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && state.status === "error") {
+      alert(state.message);
+    } else if (state && state.status === "success") {
+      // 성공 시 폼 초기화
+    }
+  }, [state]);
+  return (
+    <section style={{ maxWidth: "700px", margin: "30px auto" }}>
+      <form className="write-form" action={formAction}>
+        parent
+        <input name="parent" value={postId} type="hidden" readOnly />
+        author
+        <input name="author" value={author} type="hidden" readOnly />
+        <input
           name="content"
           disabled={isPending}
           required
           className="write-input"
           placeholder="댓글을 입력하세요"
-          
-          />
-           //버튼 코드 작성
-        
-        </form>
+        />
+        <button disabled={isPending} type="submit">
+          {isPending ? "...작성중" : "작성하기"}
+        </button>
+      </form>
     </section>
-)}
+  );
+}
--- 25cls_board_serveraction_start-main/app/detail/[id]/CommentItem.js
+++ 25cls_board_serveraction_start-main/app/detail/[id]/CommentItem.js
@@ -1,48 +1,51 @@
-'use client'
+"use client";
 //import CommentItemDeleteButton from "./comment-delete";
 import { deleteCommentAction } from "@/actions/delete-comment";
 import { useActionState, useEffect, useRef } from "react";
 
-export  function DeleteButton({
+export function DeleteButton({
   // DeleteButton 으로 네이밍 변경
   commentId,
   postId,
-  author
+  author,
 }) {
-   //코드 작성
-
+  //코드 작성
+  const formRef = useRef(null);
+  const [state, formAction, isPending] = useActionState(
+    deleteCommentAction,
+    null
+  );
   return (
-    <form  >//속성 추가하기
-      <input name="commentId" value={commentId} type="hidden" readOnly/>
-      <input name="postId" value={postId} type="hidden" readOnly/>
-      <input name="author" value={author} type="hidden" readOnly/>
+    <form>
+      <input name="commentId" value={commentId} type="hidden" readOnly />
+      <input name="postId" value={postId} type="hidden" readOnly />
+      <input name="author" value={author} type="hidden" readOnly />
       {isPending ? (
         <div>삭제중...</div>
       ) : (
-        <div onClick={() => formRef.current?.requestSubmit()}>
-          삭제하기
-        </div>
+        <div onClick={() => formRef.current?.requestSubmit()}>삭제하기</div>
       )}
     </form>
   );
 }
 
-export default function CommentItem({_id, content, author,parent}){
-   
-  
+export default function CommentItem({ _id, content, author, parent }) {
+  const { data: session } = useSession();
+  console.log("useSession", session);
+
   return (
-    <div className="comment-card" >
+    <div className="comment-card">
       <div className="comment-meta">
-        <span className="comment-author">{ author}</span>
+        <span className="comment-author">{author}</span>
       </div>
       <div className="comment-content-box">
-        <span className="mention">{content}</span> 
+        <span className="mention">{content}</span>
       </div>
       <div className="comment-actions">
-     
-     // DeleteButton 컴포넌트 추가
+        {session?.user?.email && author ? (
+          <DeleteButton commentId={_id} postId={parent} author={author} />
+        ) : null}
       </div>
     </div>
   );
-
-} 
+}
