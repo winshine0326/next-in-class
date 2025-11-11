162032a947865640d15cecbb109bf5650dc2e048
winshine0326 <shane10340@gmail.com>
2025-11-11 10:24:24 +0900

    1-2. 댓글 삭제 액션 (delete-comment.js)

--- 25cls_board_serveraction_start-main/app/actions/delete-comment.js
+++ 25cls_board_serveraction_start-main/app/actions/delete-comment.js
@@ -3,14 +3,30 @@
 import { revalidateTag } from "next/cache";
 
 export async function deleteCommentAction(state, formData) {
- //1. formData에서 commentId,postId,author저장하기
- 
- //2.commentId이용하여 DELETE (api/comment/list/commentId)
-  
- //3. 새로고침(revalidateTag 이용)
+  const commentId = formData.get("commentId")?.toString();
+  const postId = formData.get("postId")?.toString();
+  const author = formData.get("author")?.toString();
+  //1. formData에서 commentId,postId,author저장하기
 
- //4. 서버액션 상태 리턴
+  try {
+    //2.commentId이용하여 DELETE (api/comment/list/commentId)
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/comment/list/${commentId}`,
+      {
+        method: "DELETE",
+      }
+    );
 
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    //3. 새로고침(revalidateTag option값:`comment-${postId}`)
+
+    return {
+      //4. 서버액션상태 리턴
+      status: true,
+      error: "",
+    };
   } catch (err) {
     return {
       status: false,
